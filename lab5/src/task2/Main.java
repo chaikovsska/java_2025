2713a6480e809ca93e438a058b2970ee1d82af4a
Chaikovska Sofiia <chaikovska.sofiia@lll.kpi.ua>
2025-12-10 22:57:59 +0200

    added lab5

---
+++ lab5/src/task2/Main.java
@@ -0,0 +1,10 @@
+package task2;
+
+import task2.controller.ShapeController;
+
+public class Main {
+    public static void main(String[] args) {
+        ShapeController controller = new ShapeController();
+        controller.run();
+    }
+}
